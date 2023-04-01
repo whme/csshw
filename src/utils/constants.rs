85ac8b847b575c4eb96a7f48446cee2bad7b1f1a
whme <whme.dev@gmail.com>
2023-04-01 19:54:39 +0200

    Add link to PIPE_NAME documentation

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,4 +1,5 @@
 use std::env;
 
 pub const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+// https://learn.microsoft.com/en-us/windows/win32/ipc/pipe-namess
 pub const PIPE_NAME: &str = concat!(r"\\.\pipe\", env!("CARGO_PKG_NAME"), "-named-pipe-for-ipc");
