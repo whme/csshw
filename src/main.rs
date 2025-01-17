804f8f424f82825fdce10fa3e874adee24885399
whme <whme.dev@gmail.com>
2025-01-18 00:43:01 +0100

    Add docstrings to lib and main

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,16 @@ use csshw::{
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
 /// Cluster SSH tool for Windows inspired by csshX
+///
+/// The main CLI arguments
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
+    /// Optional subcommand
+    /// Usually not specified by the user
     #[clap(subcommand)]
     command: Option<Commands>,
-    /// Username used to connect to the hosts
+    /// Optional username used to connect to the hosts
     #[clap(short, long)]
     username: Option<String>,
     /// Hosts to connect to
@@ -29,8 +33,13 @@ struct Args {
     debug: bool,
 }
 
+/// The ``command`` CLI subcommand
 #[derive(Debug, Subcommand)]
 enum Commands {
+    /// Subcommand that will launch a single client window
+    ///
+    /// connecting to the given host with the given username.
+    /// It will also try to read input from a daemon via the named pipe.
     Client {
         /// Username used to connect to the host
         #[clap(long, short = 'u')]
@@ -38,16 +47,35 @@ enum Commands {
         /// Host to connect to
         host: String,
     },
+    /// Subcommand that will launch the daemon window.
+    ///
+    /// The daemon is responsible to launch the client windows,
+    /// one for each given host.
+    /// For each client a named pipe will be created and any keystrokes
+    /// the daemon window receives are forwarded via the pipes to all the clients.
+    /// Also handles control mode.
     Daemon {
         /// Username used to connect to the hosts
         #[clap(long, short = 'u')]
         username: Option<String>,
-
         /// Host(s) to connect to
         hosts: Vec<String>,
     },
 }
 
+/// Resolve cluster tags into hostnames
+///
+/// Iterates over the list of hosts to find and resolve cluster tags.
+/// Nested cluster tags are supported but recursivness is not checked for.
+///
+/// # Arguments
+///
+/// * `hosts`       - List of hosts including hostnames and or cluster tags
+/// * `clusters`    - List of available cluster tags
+///
+/// # Returns
+///
+/// A list of hostnames
 fn resolve_cluster_tags<'a>(hosts: Vec<&'a str>, clusters: &'a Vec<Cluster>) -> Vec<&'a str> {
     let mut resolved_hosts: Vec<&str> = Vec::new();
     let mut is_cluster_tag: bool;
@@ -70,6 +98,12 @@ fn resolve_cluster_tags<'a>(hosts: Vec<&'a str>, clusters: &'a Vec<Cluster>) -> 
     return resolved_hosts;
 }
 
+/// The main entrypoint
+///
+/// Parses the CLI arguments,
+/// loads an existing config or writes the default config to disk, and
+/// calls the respective subcommand.
+/// If no subcommand is given we launch the daemon subcommand in a new window.
 #[tokio::main]
 async fn main() {
     match std::env::current_exe() {
