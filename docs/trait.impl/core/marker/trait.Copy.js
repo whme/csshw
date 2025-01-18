939a6c689b06e8c8b8803594e68b60aabab75e7d
whme <whme.dev@gmail.com>
2025-01-18 22:03:32 +0100

    Do not include source in html

--- docs/trait.impl/core/marker/trait.Copy.js
+++ docs/trait.impl/core/marker/trait.Copy.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["csshw",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"csshw/daemon/workspace/enum.Scaling.html\" title=\"enum csshw::daemon::workspace::Scaling\">Scaling</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"csshw/daemon/workspace/struct.WorkspaceArea.html\" title=\"struct csshw::daemon::workspace::WorkspaceArea\">WorkspaceArea</a>"]]],["csshw_lib",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"csshw_lib/daemon/workspace/enum.Scaling.html\" title=\"enum csshw_lib::daemon::workspace::Scaling\">Scaling</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"csshw_lib/daemon/workspace/struct.WorkspaceArea.html\" title=\"struct csshw_lib::daemon::workspace::WorkspaceArea\">WorkspaceArea</a>"]]]]);
+    var implementors = Object.fromEntries([["csshw_lib",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"csshw_lib/daemon/workspace/enum.Scaling.html\" title=\"enum csshw_lib::daemon::workspace::Scaling\">Scaling</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"csshw_lib/daemon/workspace/struct.WorkspaceArea.html\" title=\"struct csshw_lib::daemon::workspace::WorkspaceArea\">WorkspaceArea</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[585,606]}
+//{"start":57,"fragment_lengths":[605]}
