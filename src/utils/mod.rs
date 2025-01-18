466305e1b3321f659916f93b4cae7dc9d17646e6
whme <whme.dev@gmail.com>
2025-01-18 14:24:41 +0100

    Add docstrings to utils/

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,6 +4,7 @@ use std::{mem, ptr, thread, time};
 use windows::core::HSTRING;
 use windows::Win32::Foundation::{COLORREF, HANDLE, HWND, RECT};
 use windows::Win32::Graphics::Dwm::{DwmSetWindowAttribute, DWMWA_BORDER_COLOR};
+use windows::Win32::System::Console::KEY_EVENT as KEY_EVENT_U32;
 use windows::Win32::System::Console::{
     FillConsoleOutputAttribute, GetConsoleScreenBufferInfo, GetConsoleWindow, GetStdHandle,
     ReadConsoleInputW, ScrollConsoleScreenBufferW, SetConsoleCursorPosition,
@@ -21,8 +22,18 @@ pub mod config;
 pub mod constants;
 pub mod debug;
 
-const KEY_EVENT: u16 = 1;
+/// u16 representation of a [KEY_EVENT][1].
+///
+/// For some reason the public [KEY_EVENT][1] constant is a u32
+/// while the [INPUT_RECORD][2].`EventType` is u16...
+///
+/// [1]: https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Console/constant.KEY_EVENT.html
+/// [2]: https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Console/struct.INPUT_RECORD.html
+const KEY_EVENT: u16 = KEY_EVENT_U32 as u16;
 
+/// Continously prints the window rectangle of the current console window.
+///
+/// Intended use for debugging only.
 pub fn print_console_rect() {
     loop {
         let mut window_rect = RECT::default();
@@ -32,12 +43,22 @@ pub fn print_console_rect() {
     }
 }
 
+/// Sets the window title of the current console window.
+///
+/// # Arguments
+///
+/// * `title` - The string to be set as window title.
 pub fn set_console_title(title: &str) {
     unsafe {
         SetWindowTextW(GetConsoleWindow(), &HSTRING::from(title)).unwrap();
     }
 }
 
+/// Sets the back- and foreground color of the current console window.
+///
+/// # Arguments
+///
+/// * `color` - The color value describing the back- and foreground color.
 pub fn set_console_color(color: CONSOLE_CHARACTER_ATTRIBUTES) {
     unsafe {
         SetConsoleTextAttribute(get_console_output_buffer(), color).unwrap();
@@ -59,6 +80,7 @@ pub fn set_console_color(color: CONSOLE_CHARACTER_ATTRIBUTES) {
     }
 }
 
+/// Empties the console screen output buffer of the current console window.
 pub fn clear_screen() {
     let mut buffer_info = CONSOLE_SCREEN_BUFFER_INFO::default();
     let console_output_handle = get_console_output_buffer();
@@ -98,6 +120,22 @@ pub fn clear_screen() {
     }
 }
 
+/// Sets the border color of the current console window.
+///
+/// Windows10 does not support this.
+///
+/// # Arguments
+///
+/// * `color` - RGB [COLORREF][1] to set as border color.
+///
+/// # Examples
+///
+/// ```
+/// // Note: inversed order of RGB        BBGGRR
+/// set_console_border_color(COLORREF(0x001A2B3C));
+/// ```
+///
+/// [1]: https://learn.microsoft.com/en-us/windows/win32/gdi/colorref
 pub fn set_console_border_color(color: COLORREF) {
     if !is_windows_10() {
         unsafe {
@@ -112,10 +150,20 @@ pub fn set_console_border_color(color: COLORREF) {
     }
 }
 
+/// Returns the title of the current console window.
 pub fn get_console_title() -> String {
     return get_window_title(unsafe { &GetConsoleWindow() });
 }
 
+/// Returns the title of the window represented by the given window handle [HWND].
+///
+/// # Arguments
+///
+/// * `handle` - Reference to a window handle for which to retrieve the window title.
+///
+/// # Returns
+///
+/// The title of the window.
 pub fn get_window_title(handle: &HWND) -> String {
     let mut title: [u16; MAX_WINDOW_TITLE_LENGTH] = [0; MAX_WINDOW_TITLE_LENGTH];
     unsafe {
@@ -131,6 +179,16 @@ pub fn get_window_title(handle: &HWND) -> String {
     });
 }
 
+/// Returns a [HANDLE] to the requested [STD_HANDLE] of the current process.
+///
+/// # Arguments
+///
+/// * `nstdhandle` - The standard handle to retrieve.
+///                  Either [STD_INPUT_HANDLE] or [STD_OUTPUT_HANDLE].
+///
+/// # Returns
+///
+/// The [HANDLE] to the requested [STD_HANDLE].
 fn get_std_handle(nstdhandle: STD_HANDLE) -> HANDLE {
     return unsafe {
         GetStdHandle(nstdhandle)
@@ -138,14 +196,19 @@ fn get_std_handle(nstdhandle: STD_HANDLE) -> HANDLE {
     };
 }
 
+/// Returns a [HANDLE] to the [STD_INPUT_HANDLE] of the current process.
 pub fn get_console_input_buffer() -> HANDLE {
     return get_std_handle(STD_INPUT_HANDLE);
 }
 
+/// Returns a [HANDLE] to the [STD_OUTPUT_HANDLE] of the current process.
 pub fn get_console_output_buffer() -> HANDLE {
     return get_std_handle(STD_OUTPUT_HANDLE);
 }
 
+/// Returns a single [INPUT_RECORD] read from the current process stdinput.
+///
+/// Blocks until 1 record was read.
 fn read_console_input() -> INPUT_RECORD {
     const NB_EVENTS: usize = 1;
     let mut input_buffer: [INPUT_RECORD; NB_EVENTS] = [INPUT_RECORD::default(); NB_EVENTS];
@@ -166,6 +229,9 @@ fn read_console_input() -> INPUT_RECORD {
     return input_buffer[0];
 }
 
+/// Returns a single [INPUT_RECORD_0] where `EventType` == [KEY_EVENT].
+///
+/// Blocks until 1 key event record was read.
 pub fn read_keyboard_input() -> INPUT_RECORD_0 {
     loop {
         let input_record = read_console_input();
@@ -180,6 +246,18 @@ pub fn read_keyboard_input() -> INPUT_RECORD_0 {
     }
 }
 
+/// Changes size and position of the current console window.
+///
+/// # Arguments
+///
+/// * `x`       - The x coordinate to move the window to.
+///               From the top left corner of the screen.
+/// * `y`       - The y coordinate to move the window to.
+///               From the top left corner of the screen.
+/// * `width`   - The width in pixels to resize the window to.
+///               In logical scaling.
+/// * `height`  - The height in pixels to resize the window to.
+///               In logical scaling.
 pub fn arrange_console(x: i32, y: i32, width: i32, height: i32) {
     // FIXME: sometimes a daemon or client console isn't being arrange correctly
     // when this simply retrying doesn't solve the issue. Maybe it has something to do
@@ -189,6 +267,13 @@ pub fn arrange_console(x: i32, y: i32, width: i32, height: i32) {
     }
 }
 
+/// Detects if the current windows installation is Windows 10 or not.
+///
+/// Uses the os version, Windows 10 is <= `10._.22000`.
+///
+/// # Returns
+///
+/// Whether the current windows installation is Windows 10 or not.
 pub fn is_windows_10() -> bool {
     let version = os_info::get().version().to_string();
     let mut iter = version.split('.');
