d31f1cc5086e371bfc8c79cf3a94cf346d558ba4
whme <whme.dev@gmail.com>
2025-01-18 15:27:45 +0100

    Add docstrings to serde/

--- src/serde/mod.rs
+++ src/serde/mod.rs
@@ -1,4 +1,7 @@
 pub mod deserialization;
 pub mod serialization;
 
+/// Lenght of a serialized [INPUT_RECORD_0][1]
+///
+/// [1]: https://microsoft.github.io/windows-docs-rs/doc/windows/Win32/System/Console/union.INPUT_RECORD_0.html
 pub const SERIALIZED_INPUT_RECORD_0_LENGTH: usize = 18;
--- src/serde/serialization.rs
+++ src/serde/serialization.rs
@@ -1,11 +1,14 @@
 use rmp::encode::ByteBuf;
 use windows::Win32::System::Console::{INPUT_RECORD_0, KEY_EVENT_RECORD, KEY_EVENT_RECORD_0};
 
+/// Serialize a struct into a [ByteBuf].
 pub trait Serialize {
+    /// Returns a serialized self as [ByteBuf].
     fn serialize(&self) -> ByteBuf;
 }
 
 impl Serialize for KEY_EVENT_RECORD_0 {
+    /// Returns the u16 `UnicodeChar` in a [ByteBuf].
     fn serialize(&self) -> ByteBuf {
         let mut buf = ByteBuf::new();
         rmp::encode::write_u16(&mut buf, unsafe { self.UnicodeChar }).unwrap();
@@ -14,6 +17,10 @@ impl Serialize for KEY_EVENT_RECORD_0 {
 }
 
 impl Serialize for KEY_EVENT_RECORD {
+    /// Returns the [KEY_EVENT_RECORD] as [ByteBuf] in the following layout:
+    /// ```
+    /// [bool KeyDown, u16 ReapetCount, u16 VirtualKeyCode, u16 VirtualScanCode, u16 UnicodeChar, u32 ControlKeyState]
+    /// ```
     fn serialize(&self) -> ByteBuf {
         let mut buf = ByteBuf::new();
         rmp::encode::write_bool(&mut buf, self.bKeyDown.as_bool()).unwrap();
@@ -27,6 +34,9 @@ impl Serialize for KEY_EVENT_RECORD {
 }
 
 impl Serialize for INPUT_RECORD_0 {
+    /// Returns the [INPUT_RECORD_0].`KeyEvent` serialized as [ByteBuf].
+    ///
+    /// Panics if the [INPUT_RECORD_0] is not a `KeyEvent`.
     fn serialize(&self) -> ByteBuf {
         return unsafe { self.KeyEvent }.serialize();
     }
