63d4eba9b4d39dd3ff53da1524b0e977fa9d701d
whme <whme.dev@gmail.com>
2025-01-20 20:32:17 +0100

    Add tests for `resolve_cluster_tags`

--- src/tests/test_daemon.rs
+++ src/tests/test_daemon.rs
@@ -1,1 +1,45 @@
+mod daemon_test {
+    use crate::{daemon::resolve_cluster_tags, utils::config::Cluster};
 
+    #[test]
+    fn test_resolve_cluster_tags() {
+        let hosts: Vec<&str> = vec!["host0", "cluster1", "host3", "host0", "host1"];
+        let clusters: Vec<Cluster> = vec![Cluster {
+            name: "cluster1".to_string(),
+            hosts: vec!["host1".to_string(), "host2".to_string()],
+        }];
+        assert_eq!(
+            resolve_cluster_tags(hosts, &clusters),
+            vec!["host0", "host1", "host2", "host3", "host0", "host1"]
+        );
+    }
+
+    #[test]
+    fn test_resolve_cluster_tags_no_cluster() {
+        let hosts: Vec<&str> = vec!["host0"];
+        let clusters: Vec<Cluster> = vec![Cluster {
+            name: "cluster1".to_string(),
+            hosts: vec!["host1".to_string(), "host2".to_string()],
+        }];
+        assert_eq!(resolve_cluster_tags(hosts, &clusters), vec!["host0"]);
+    }
+
+    #[test]
+    fn test_resolve_cluster_tags_simple_nested_cluster() {
+        let hosts: Vec<&str> = vec!["cluster2"];
+        let clusters: Vec<Cluster> = vec![
+            Cluster {
+                name: "cluster1".to_string(),
+                hosts: vec!["host1".to_string(), "host2".to_string()],
+            },
+            Cluster {
+                name: "cluster2".to_string(),
+                hosts: vec!["cluster1".to_owned(), "host3".to_owned()],
+            },
+        ];
+        assert_eq!(
+            resolve_cluster_tags(hosts, &clusters),
+            vec!["host1", "host2", "host3"]
+        );
+    }
+}
