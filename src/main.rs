409e3a15a345336870812115d0d6d0e6ded48d70
whme <whme.dev@gmail.com>
2025-01-26 18:00:18 +0100

    Add unit tests for CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,11 @@
 #![warn(missing_docs)]
 #![doc(html_no_source)]
 
-use csshw_lib::cli;
+use clap::Parser as _;
+use csshw_lib::cli::{self, Args};
 
 /// The main entrypoint of the binary
 #[tokio::main]
 async fn main() {
-    cli::entrypoint().await;
+    cli::entrypoint(Args::parse()).await;
 }
