3934ae0c5eba272cdd7576419670745df6f36be6
whme <whme.dev@gmail.com>
2025-11-01 18:12:49 +0100

    Fix test_mod

--- src/tests/utils/test_mod.rs
+++ src/tests/utils/test_mod.rs
@@ -2,10 +2,3 @@
 
 #![deny(clippy::implicit_return)]
 #![allow(clippy::needless_return, clippy::doc_overindented_list_items)]
-
-// Import the windows tests module
-#[path = "test_windows.rs"]
-mod test_windows;
-
-// Note: All Windows API related tests have been moved to test_windows.rs
-// This file now serves as the main test module entry point for utils tests
