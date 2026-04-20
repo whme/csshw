e8399285c3cf888edf88ca2b032b4f93900b9d52
whme <whme.dev@gmail.com>
2026-04-20 20:16:40 +0200

    New csshw Icon
    Replaces .ico with .svg
    
    GitHub: #149

--- build.rs
+++ build.rs
@@ -1,6 +1,15 @@
+use std::path::Path;
+use svg_to_ico::svg_to_ico;
 extern crate embed_resource;
 
 fn main() {
+    svg_to_ico(
+        Path::new("res/csshw.svg"),
+        96.0,
+        Path::new("res/csshw.ico"),
+        &[16, 24, 32, 48, 64, 128, 256],
+    )
+    .expect("Failed to convert SVG to ICO");
     embed_resource::compile_for("res/csshw.rc", ["csshw"], embed_resource::NONE)
         .manifest_required()
         .unwrap();
